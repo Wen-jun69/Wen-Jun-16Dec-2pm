a7db13668d264a39810497ab9f056de3452b3ec5
Wen-jun69 <soonwenjun@gmail.com>
2024-12-19 07:17:29 +0000

    Day 4

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,4 +1,100 @@
 # Write all your codes for Day 3 here.
 # REMEMBER to change main.py to import this file.
-# You can COMMENT out the previous task before going on to the next task
-print("hello from day3")
+# You can COMMENT out the previous task before going on to the next task.
+
+# name = input("What is your name? ")
+
+# if name == "sussy baka":
+#     print("Sussy baka killed 69 crewmates." )
+
+# num_pens = input("How many pens did you buy? ")
+
+# if num_pens == "69":
+#     print("eiwrjwoiehfnowehfuewfhwieufhewiufhwiufhei." )
+
+# print(name +  " bought " +  num_pens + " pens. ")
+
+# below: the cool calculator!
+
+# type= input("Do you want to add, multiply, divide or subtract? ")
+
+# if type == "add":
+#     num1= int(input("What is the first number to add? "))
+#     num2= int(input("What is the second number to add? "))
+
+#     ans = num1 + num2
+
+#     print(str( num1 ) + " + " + str( num2 ) + " = "  + str(ans))
+
+# if type == "multiply":
+#     num1= int(input("What is the first number to multiply? "))
+#     num2= int(input("What is the second number to multiply? "))
+
+#     ans = num1 * num2
+
+#     print(str( num1 ) + " x " + str( num2 ) + " = "  + str(ans))
+
+# if type == "divide":
+#     num1= int(input("What is the first number to divide? "))
+#     num2= int(input("What is the second number to divide? "))
+
+#     ans = num1 / num2
+
+#     print(str( num1 ) + " / " + str( num2 ) + " = "  + str(ans))
+
+# if type == "subtract":
+#     num1= int(input("What is the first number to subtract? "))
+#     num2= int(input("What is the second number to subtract? "))
+
+#     ans = num1 - num2
+
+#     print(str( num1 ) + " - " + str( num2 ) + " = "  + str(ans))
+
+# age1 = int(input("How old is Person A? "))
+# age2 = int(input("How old is Person B? "))
+
+# if age1 > age2:
+#     print("Person A is older than Person B.") 
+# else:
+#     print("Person A is not older than Person B." )
+
+# the password game!!!!!!
+
+# password = "sususus"
+# UserGuess = "iuehdiueh"
+# lives = 3
+# for i in range(3):
+#     if UserGuess != password:
+#         UserGuess = input("What is the password? ")
+#         if lives > 0:
+#             if UserGuess == password:
+#                 print("cool")
+#             else:
+#                 print("DIE!!!!")
+#                 lives = lives - 1
+
+# if lives <= 0:
+#     print("System has been locked out......")
+
+# import random
+# for i in range(10):
+#     num1 = random.randint(1,10)
+#     num2 = random.randint(1,10)
+#     ans = num1 + num2
+#     score = 0
+
+#         UserAns = int(input("What is " + str(num1) + " + " + str(num2) +  "? "))
+
+#         if UserAns == ans:
+#             print("yay!!!!!")
+#             score = score + 1
+#         else:
+#             print("game over!!! " + " score: " + str(score))
+
+
+
+            
+  
+
+    
+    
