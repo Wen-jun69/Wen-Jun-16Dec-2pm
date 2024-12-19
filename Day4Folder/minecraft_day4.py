bc3af0b21d08746fba30d28629e9d4c7c1df1f14
Wen-jun69 <soonwenjun@gmail.com>
2024-12-19 08:57:30 +0000

    we

--- Day4Folder/minecraft_day4.py
+++ Day4Folder/minecraft_day4.py
@@ -1,108 +1,108 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
-def come():
-    agent.teleport_to_player()
+# def come():
+#     agent.teleport_to_player()
 
 
-player.on_chat("come", come)
+# player.on_chat("come", come)
 
-def fw(num):
-    agent.move(FORWARD, num)
+# def fw(num):
+#     agent.move(FORWARD, num)
 
-player.on_chat("fw", fw)
-
-
-def bk(num):
-    agent.move(BACK, num)
-
-player.on_chat("bk", bk)
-
-def ml(num):
-    agent.move(LEFT, num)
-
-player.on_chat("ml", ml)
+# player.on_chat("fw", fw)
 
 
-def mr(num):
-    agent.move(RIGHT, num)
+# def bk(num):
+#     agent.move(BACK, num)
 
-player.on_chat("mr", mr)
+# player.on_chat("bk", bk)
+
+# def ml(num):
+#     agent.move(LEFT, num)
+
+# player.on_chat("ml", ml)
 
 
-def up(num):
-    agent.move(UP, num)
+# def mr(num):
+#     agent.move(RIGHT, num)
 
-player.on_chat("up", up)
+# player.on_chat("mr", mr)
 
 
-def down(num):
-    agent.move(DOWN, num)
+# def up(num):
+#     agent.move(UP, num)
 
-player.on_chat("down", down)
+# player.on_chat("up", up)
 
-def tl():
-    agent.turn(LEFT)
 
-player.on_chat("tl", tl)
+# def down(num):
+#     agent.move(DOWN, num)
 
-def tr():
-    agent.turn(RIGHT)
+# player.on_chat("down", down)
 
-player.on_chat("tr", tr)
+# def tl():
+#     agent.turn(LEFT)
 
-def chop(num):
-    for i in range(num):
-        agent.destroy(FORWARD)
-        agent.collect_all()
-        agent.destroy(UP)
-        agent.move(UP, 1)
+# player.on_chat("tl", tl)
 
-player.on_chat("chop", chop)
+# def tr():
+#     agent.turn(RIGHT)
 
-def mine(num):
-    for i in range(num):
-        agent.destroy(FORWARD)
-        agent.destroy(LEFT)
-        agent.destroy(RIGHT)
-        agent.destroy(DOWN)
-        agent.collect_all()
-        agent.move(FORWARD, 1)
+# player.on_chat("tr", tr)
 
-player.on_chat("mine", mine)
+# def chop(num):
+#     for i in range(num):
+#         agent.destroy(FORWARD)
+#         agent.collect_all()
+#         agent.destroy(UP)
+#         agent.move(UP, 1)
 
-def build(height,width):
-    for i in range(width):
-        for i in range(height):
-            agent.place(FORWARD)
-            agent.move(UP, 1)
-        agent.move(DOWN, 3)
-        agent.move(RIGHT, 1)
+# player.on_chat("chop", chop)
 
-player.on_chat("wall", build)
+# def mine(num):
+#     for i in range(num):
+#         agent.destroy(FORWARD)
+#         agent.destroy(LEFT)
+#         agent.destroy(RIGHT)
+#         agent.destroy(DOWN)
+#         agent.collect_all()
+#         agent.move(FORWARD, 1)
 
-def solvespiral():
-    while agent.inspect(AgentInspection.BLOCK, DOWN) != DIAMOND_BLOCK:
-        if not agent.detect(AgentDetection.BLOCK, RIGHT):
-            agent.turn(RIGHT)
+# player.on_chat("mine", mine)
 
-        agent.move(FORWARD, 1)
+# def build(height,width):
+#     for i in range(width):
+#         for i in range(height):
+#             agent.place(FORWARD)
+#             agent.move(UP, 1)
+#         agent.move(DOWN, 3)
+#         agent.move(RIGHT, 1)
 
-player.on_chat("solvespiral", solvespiral)
+# player.on_chat("wall", build)
 
-def solvemaze():
-    while agent.detect(AgentDetection.BLOCK, DOWN):
-        if not agent.detect(AgentDetection.BLOCK, LEFT):
-            agent.turn(LEFT)
-            agent.move(FORWARD, 1)
+# def solvespiral():
+#     while agent.inspect(AgentInspection.BLOCK, DOWN) != DIAMOND_BLOCK:
+#         if not agent.detect(AgentDetection.BLOCK, RIGHT):
+#             agent.turn(RIGHT)
 
-        elif not agent.detect(AgentDetection.BLOCK, FORWARD):
-            agent.move(FORWARD, 1)
+#         agent.move(FORWARD, 1)
 
-        else:
-            agent.turn(RIGHT)
-            agent.move(FORWARD, 1)
+# player.on_chat("solvespiral", solvespiral)
 
-player.on_chat("solvemaze", solvemaze)
+# def solvemaze():
+#     while agent.detect(AgentDetection.BLOCK, DOWN):
+#         if not agent.detect(AgentDetection.BLOCK, LEFT):
+#             agent.turn(LEFT)
+#             agent.move(FORWARD, 1)
+
+#         elif not agent.detect(AgentDetection.BLOCK, FORWARD):
+#             agent.move(FORWARD, 1)
+
+#         else:
+#             agent.turn(RIGHT)
+#             agent.move(FORWARD, 1)
+
+# player.on_chat("solvemaze", solvemaze)
 
 
 
