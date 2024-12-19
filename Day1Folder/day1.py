a27a72c6ddf6392bf5a6d28cf3dd620ffc2ac0b6
Wen-jun69 <soonwenjun@gmail.com>
2024-12-19 07:16:58 +0000

    Day 4

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,4 +1,33 @@
 # Write all your codes for Day 1 here.
 # REMEMBER to change main.py to import this file.
-# You can COMMENT out the previous task before going on to the next task
-print("hello from day1")
+#You can COMMENT out the previous task before going on to the next 
+# animal1= "Cat"
+# animal2="Dog"
+# animal3 ="sheep"
+# print(animal1)
+# print(animal2)
+# print(animal3)
+
+# name= "Wen Jun"
+# food= "french fries"
+
+# print("My name is "+ name)
+# print("I like eating "+ food)
+
+# def intro():
+#     print("I am Soon Wen Jun")
+#     print("I am 12 years old")
+#     print("I attend Teck Ghee Primary School")
+#     print("If I had $2000, I would buy the newest phone.")
+
+# intro()
+
+# def addition(number1, number2):
+#     print(number1 + number2)
+
+# addition(1,4)
+
+# if addition == 3:
+#     print("sus")
+# else:
+#     print("too bad")
